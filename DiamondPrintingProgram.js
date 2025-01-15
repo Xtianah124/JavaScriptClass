aa66c6ddc84767569a289c48946abb5d48a00749
Xtianah124 <oludechristianah16@gmail.com>
2025-01-15 12:15:11 +0100

    javaScriptClass

--- DiamondPrintingProgram.js
+++ DiamondPrintingProgram.js
@@ -1,6 +1,24 @@
-function asterisks(asterisk){
-    let asterisk;
-    for(let i = 0; i < 5; i++){
+function asterisks(size) {
+    let asterisk = "";
+
+    for (let i = 1; i <= size; i++) {
+        for (let j = 0; j <= size - 1; j++) {
+            asterisk += ' ';
+        }
 
     }
+
+    for (let i = 1; i <= 2 * i; i++) {
+        asterisk += '*';
+    }
+    asterisk += '\n';
+
+    for (let i = size - 1; i >= 1; i--) {
+        for (let j = 1; j <= size; j++) {
+            asterisk += '*';
+        }
+        asterisk += '\n';
+    }
+    console.log(asterisk);
 }
+asterisks(10);
